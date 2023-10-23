c07bbd1fbc58bb495401e6dddec222f5c344ed10
Nenad Ilic <nenadilic84@gmail.com>
2023-10-23 08:37:30 +0000

    Adding rosbag2_play and fwe artifacts

--- app.py
+++ app.py
@@ -18,8 +18,14 @@ yocto_sdk_s3_path = app.node.try_get_context("yoctoSdkS3Path")
 if yocto_sdk_s3_path is None:
     raise Exception("Context parameter 'yoctoSdkS3Path' must be supplied")
 
+# Fetch and check the existence of the yoctoSdkS3Path context parameter
+s3_fwe_artifacts = app.node.try_get_context("s3FweArtifacts")
+if s3_fwe_artifacts is None:
+    raise Exception("Context parameter 's3FweArtifacts' must be supplied")
+
 # List of repository names
 repository_names = ["fleetwise_edge_connector",
+                    "rosbag2_play",
                     "can_data_analyzer_publisher",
                     "rtos_app_data_publisher",
                     "rtos_os_data_publisher",
@@ -31,6 +37,7 @@ for repo_name in repository_names:
     Ggv2PipelineStack(app, f"greengrass-components-pipeline-{repo_name.replace('_', '-')}",
                       repository_name=repo_name,
                       yocto_sdk_s3_path=yocto_sdk_s3_path,
+                      s3_fwe_artifacts=s3_fwe_artifacts,
                       s3_gg_components_prefix="gg",
                       env=cdk.Environment(
                         account=os.getenv('CDK_DEFAULT_ACCOUNT'),
