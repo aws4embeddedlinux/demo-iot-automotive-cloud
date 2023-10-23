842b005ca3329a0eb472a9725ee3b23ce16815ca
alinadima <dimaalin@amazon.de>
2023-10-23 15:10:24 +0200

    Fixes to the missing role and README file.

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ MainStack(app, "biga-aws-iotfleetwise",
             region='us-west-2'))
 
 # Fetch and check the existence of the yoctoSdkS3Path context parameter
-yocto_sdk_s3_path = app.node.try_get_context("yoctoSdkS3Path")
-if yocto_sdk_s3_path is None:
-    raise Exception("Context parameter 'yoctoSdkS3Path' must be supplied")
+#yocto_sdk_s3_path = app.node.try_get_context("yoctoSdkS3Path")
+#if yocto_sdk_s3_path is None:
+    #raise Exception("Context parameter 'yoctoSdkS3Path' must be supplied")
 
 # Fetch and check the existence of the yoctoSdkS3Path context parameter
 s3_fwe_artifacts = app.node.try_get_context("s3FweArtifacts")
@@ -24,19 +24,23 @@ if s3_fwe_artifacts is None:
     raise Exception("Context parameter 's3FweArtifacts' must be supplied")
 
 # List of repository names
+#repository_names = ["fleetwise_edge_connector",
+                  #  "rosbag2_play",
+                  #  "can_data_analyzer_publisher",
+                  #  "rtos_app_data_publisher",
+                  #  "rtos_os_data_publisher",
+                  #  "virtual_can_forwarder",
+                  #  "ipcf_shared_memory"]
+# List of repository names
 repository_names = ["fleetwise_edge_connector",
-                    "rosbag2_play",
-                    "can_data_analyzer_publisher",
-                    "rtos_app_data_publisher",
-                    "rtos_os_data_publisher",
-                    "virtual_can_forwarder",
-                    "ipcf_shared_memory"]
+                    "rosbag2_play"]
 
 # Create stacks for each repository
 for repo_name in repository_names:
     Ggv2PipelineStack(app, f"greengrass-components-pipeline-{repo_name.replace('_', '-')}",
                       repository_name=repo_name,
-                      yocto_sdk_s3_path=yocto_sdk_s3_path,
+                      #yocto_sdk_s3_path=yocto_sdk_s3_path,
+                      yocto_sdk_s3_path="",
                       s3_fwe_artifacts=s3_fwe_artifacts,
                       s3_gg_components_prefix="gg",
                       env=cdk.Environment(
