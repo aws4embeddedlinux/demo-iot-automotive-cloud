99ee7e58b5992acfdd41daac4105a213e1416d04
Nenad Ilic <nenadilic84@gmail.com>
2023-11-22 15:19:18 +0000

    update ipcf-replacement

--- greengrass_components/ipcf_shared_memory_replacement/ipcf_shared_memory_replacement.py
+++ greengrass_components/ipcf_shared_memory_replacement/ipcf_shared_memory_replacement.py
@@ -6,46 +6,68 @@ from awsiot.greengrasscoreipc.model import (
     PublishMessage,
     BinaryMessage
 )
-from time import sleep
 import time
 import traceback
+import threading
 
-# global CAN read success counter
+import argparse
+
+# Global variables for data aggregation
 successCount = 0
+aggregateData = {
+    'successCount': 0,
+    'errorCount': 0
+}
+
+TOPIC = 'topic/localGGProc'
 
 def processCan(can):
-            # this will consumed by the greengrass_stats_publisher
-            topic= 'topic/localGGProc'
+    global aggregateData
+    aggregateData['successCount'] += 1
 
-            # must be <= 256
-            dataSize = 256
+def publish_aggregated_data():
+    global ipc_client, aggregateData
 
-            # we assume there is no read error
-            errorCount = 0
+    while True:
+        time.sleep(int(args.timeout))  # Wait for 10 seconds before publishing
+        epoch_time = int(time.time())
 
-            # processCAN is called for every single CAN message
-            global successCount
-            successCount += 1
+        payload = (
+            f"PROC\nDataSize 256\nSuccess {aggregateData['successCount']}\n"
+            f"Error {aggregateData['errorCount']}\nPreTS {epoch_time}"
+        )
+        print(payload)
 
-            # epoch in seconds
-            epoch_time = int(time.time())
+        try:
+            publish_message = PublishMessage(binary_message=BinaryMessage(message=bytes(payload, 'utf-8')))
+            ipc_client.publish_to_topic(topic=TOPIC, publish_message=publish_message)
+            print(f'Successfully published to topic: {TOPIC}')
+        except Exception:
+            print('Exception occurred')
+            traceback.print_exc()
 
-            payload = "PROC\nDataSize " + str(dataSize) + "\nSuccess " + str(successCount) + "\nError " + str(errorCount)+"\nPreTS "+ str(epoch_time)
-            print (payload)
-            try:
-                publish_message = PublishMessage(binary_message=BinaryMessage(message=bytes(payload, 'utf-8')))
-                ipc_client.publish_to_topic(topic=topic, publish_message=publish_message)
-                print('Successfully published to topic: ' + topic)
-            except Exception:
-                print('Exception occurred')
-                traceback.print_exc()
+        # Reset aggregated data
+        aggregateData = {
+            'successCount': 0,
+            'errorCount': 0
+        }
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--timeout", default=10)
+args = parser.parse_args()
 
 try:
-    sleep(15)
     ipc_client = GreengrassCoreIPCClientV2()
     bus = can.interface.Bus(channel='can0', bustype='socketcan')
-    notifier = can.Notifier(bus,[processCan])
-except:
+    notifier = can.Notifier(bus, [processCan])
+
+    # Start the periodic data publish thread
+    threading.Thread(target=publish_aggregated_data, daemon=True).start()
+
+except Exception:
     print("Failed to connect to CAN, should retry \n")
+    traceback.print_exc()
+
+# Main loop
 while True:
-    sleep(1)
+    time.sleep(1)  # To keep the main thread alive
